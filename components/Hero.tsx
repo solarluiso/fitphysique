855b743f67396dd55dbcd5a74a2d5e22eccf9bcd
Luis <solarluiso@gmail.com>
2024-08-27 20:35:47 -0400

    modify hero height for mobile

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import HeroSlider from "./HeroSlider";
 const Hero = () => {
   return (
     <section
-      className="h-[80vh] lg:h-[912px] bg-hero bg-cover bg-center bg-no-repeat"
+      className="h-[100vh] lg:h-[912px] bg-hero bg-cover bg-center bg-no-repeat"
       id="home"
     >
       <div className="container mx-auto h-full">
